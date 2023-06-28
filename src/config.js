e8431e5b6bc29320c9c702e261b2fa374dc937a2
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2023-06-28 14:58:31 +0200

    Add transaction funding

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 const {
+  CHAIN_ID: chainId,
   FUNDER_ACCOUNT_PRIVATE_KEY: funderAccount,
   GELATO_RELAY_API_KEY: gelatoApiKey,
   RPC_URL: rpcUrl,
 } = process.env;
 
 module.exports = {
+  chainId,
   funderAccount,
   gelatoApiKey,
   rpcUrl,
--- src/features/transactions.js
+++ src/features/transactions.js
@@ -1,6 +1,11 @@
-const fundTransaction = async (request, _) => {
-  return request.body;
-};
+const { GelatoRelay } = require('@gelatonetwork/relay-sdk');
+
+const { chainId, gelatoApiKey } = require('../config');
+
+const relay = new GelatoRelay();
+
+const fundTransaction = async (request, _) =>
+  relay.sponsoredCall({ ...request.body, chainId }, gelatoApiKey);
 
 module.exports = {
   fundTransaction,
