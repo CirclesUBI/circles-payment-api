4e4c100f67c45e631d0d8674e7222c114b45e075
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2023-06-29 09:19:17 +0200

    Add CORS

--- src/config.js
+++ src/config.js
@@ -1,12 +1,16 @@
 const {
   CHAIN_ID: chainId,
+  CORS_REGEX: corsRegex,
   FUNDER_ACCOUNT_PRIVATE_KEY: funderAccount,
   GELATO_RELAY_API_KEY: gelatoApiKey,
+  NODE_ENV: env,
   RPC_URL: rpcUrl,
 } = process.env;
 
 module.exports = {
   chainId,
+  corsRegex,
+  env,
   funderAccount,
   gelatoApiKey,
   rpcUrl,
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+const cors = require('@fastify/cors');
+
+const { corsRegex } = require('./config');
+
 // Declare all plugins registration here
 module.exports = async (fastify, _) => {
+  await fastify.register(cors, {
+    origin: [corsRegex],
+  });
+
   fastify.register(require('./routes'));
 };
