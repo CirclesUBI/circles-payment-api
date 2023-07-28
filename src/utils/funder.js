a86417b1124808208799aae723ad55d5d9bea272
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2023-07-28 17:01:32 +0200

    Fix error on prod code running

--- src/utils/funder.js
+++ src/utils/funder.js
@@ -2,9 +2,11 @@ const { ethers } = require('ethers');
 
 const { funderAccount, rpcUrl } = require('../config');
 
-const funder = new ethers.Wallet(
-  funderAccount,
-  new ethers.providers.JsonRpcProvider(rpcUrl),
-);
+const funder =
+  funderAccount &&
+  new ethers.Wallet(
+    funderAccount,
+    new ethers.providers.JsonRpcProvider(rpcUrl),
+  );
 
 module.exports = funder;
