c112b3109ba9dcdb3ffe459d710c9da3a921f22a
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2023-10-19 16:15:59 +0200

    Resolve gelato task executions to retrieve transaction hashes

--- src/features/transactions.js
+++ src/features/transactions.js
@@ -22,11 +22,34 @@ const fundLocalTransaction = (
         nonce: nonce + pendingRequests,
       }),
     )
-    .then((tx) => tx.wait())
-    .then((receipt) => ({ taskId: '0x', receipt }));
+    .then((tx) => tx.wait());
 };
 const fundGelatoTransaction = ({ body }, _) =>
-  relay.sponsoredCall({ ...body, chainId }, gelatoApiKey);
+  relay.sponsoredCall({ ...body, chainId }, gelatoApiKey).then(({ taskId }) => {
+    // Let's wait for Gelato to execute task and give us a final task state
+    const checkStatus = () =>
+      new Promise((resolve) =>
+        setTimeout(() => relay.getTaskStatus(taskId).then(resolve), 3000),
+      ).then((task) => {
+        if (task.taskState === 'ExecReverted') {
+          throw new Error('The task transaction has been reverted');
+        }
+
+        if (task.taskState === 'Cancelled') {
+          throw new Error(task.lastCheckMessage);
+        }
+
+        if (task.taskState === 'ExecSuccess') {
+          return task;
+        }
+
+        return checkStatus();
+      });
+
+    return new Promise((resolve, reject) =>
+      checkStatus().then(resolve).catch(reject),
+    );
+  });
 
 module.exports = {
   fundLocalTransaction,
